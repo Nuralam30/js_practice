0da3861dd6b8f988f4c34a4926e5f82d756e6c8c
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-04-22 20:45:42 +0600

    ludu game create

---
+++ luduGame.js
@@ -0,0 +1,29 @@
+
+var boxDiv = document.querySelector('.box');
+var container = document.querySelector('.container');
+
+// player 1 button
+const player1 = document.createElement('button');
+player1.classList.add('player1-btn');
+player1.innerHTML = "Palyer 1";
+boxDiv.appendChild(player1);
+
+// player 2 button
+const player2 = document.createElement('button');
+player2.classList.add('player2-btn');
+player2.innerHTML = "Palyer 2";
+boxDiv.appendChild(player2);
+
+// player 1 score
+const player1Text = document.createElement('p');
+player1Text.classList.add('player1-score');
+player1Text.innerHTML = "Palyer 1 score : ";
+container.appendChild(player1Text);
+
+// player 2 score
+const player2Text = document.createElement('p');
+player2Text.classList.add('player2-score');
+player2Text.innerHTML = "Palyer 2 score : ";
+container.appendChild(player2Text);
+
+
