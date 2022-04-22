5bd8a240370a01ccd09f32ca22ae54f35080f5f9
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-04-22 21:07:34 +0600

    ludu game updated

--- luduGame.js
+++ luduGame.js
@@ -14,10 +14,20 @@ player2.classList.add('player2-btn');
 player2.innerHTML = "Palyer 2";
 boxDiv.appendChild(player2);
 
+// player 1 score update
+function totalScore(){
+    player1.addEventListener('click', function(){
+        var randomScore = Math.random() * 6;
+        var roundScore = Math.round(randomScore);
+        console.log(roundScore)
+        return roundScore;
+    });
+}
+
 // player 1 score
 const player1Text = document.createElement('p');
 player1Text.classList.add('player1-score');
-player1Text.innerHTML = "Palyer 1 score : ";
+player1Text.innerHTML = "Palyer 1 score :" +totalScore();
 container.appendChild(player1Text);
 
 // player 2 score
@@ -25,5 +35,4 @@ const player2Text = document.createElement('p');
 player2Text.classList.add('player2-score');
 player2Text.innerHTML = "Palyer 2 score : ";
 container.appendChild(player2Text);
-
 
