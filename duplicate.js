f620bb1a805cb5af147575d7ba01aea08be9189b
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-05-13 18:50:24 +0600

    used different way

--- duplicate.js
+++ duplicate.js
@@ -1,5 +1,7 @@
 
-var num = [25, 45, 65, 12, 45, 86, 25, 54, 45];
+var num = [25, 45, 65, 12, 45, 86, 25, 54, 45, 27];
+
+//first way
 var uniqueNum = [];
 
 for(var i=0; i<num.length; i++){
@@ -10,5 +12,20 @@ for(var i=0; i<num.length; i++){
         uniqueNum.push(element);
     }
 }
+console.log('first way',uniqueNum)
 
-console.log(uniqueNum)
+//second way
+var uniqueNumbers = [...new Set(num)];
+console.log('second way',uniqueNumbers)
+
+
+// third way
+var uniqueSet = [];
+
+num.forEach((n) => {
+    if (!uniqueSet.includes(n)) {
+        uniqueSet.push(n);
+    }
+});
+
+console.log('third way',uniqueSet)
