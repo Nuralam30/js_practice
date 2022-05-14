f3f8baffe58d2ed8cde91f34c1c5c0c54a5f3909
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-05-14 20:16:46 +0600

    brick calculator create

---
+++ brickCalculator.js
@@ -0,0 +1,23 @@
+
+function brickCalculator(floor) {
+
+    if(floor <= 10) {
+        var totalHeight = floor * 15;
+        var totalbrick = totalHeight * 1000;
+    }
+    else if(floor <= 20) {
+        var totalHeight = ((floor - 10) * 12) + (10 * 15);
+        var totalbrick = totalHeight * 1000;
+    }
+    else if(floor <= 30) {
+        var totalHeight = ((floor - 20) * 10) + (10 * 12) + (10 * 15);
+        var totalbrick = totalHeight * 1000;
+    }
+    else {
+        var totalbrick = " Building will not be safe for living";
+    }
+    return totalbrick
+}
+
+var floorInput = brickCalculator(35);
+console.log(floorInput)
