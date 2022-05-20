e92531502d9e219b1d7cb4a8ab11423ea5771524
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-05-20 11:15:15 +0600

    multiplication Table

---
+++ multiplicationTable.js
@@ -0,0 +1,9 @@
+
+function multiplicationTable(num){
+    for( var i = 1; i<=10; i++){
+        var mul = num * i;
+        console.log(num, '*', i, '=',mul);
+    }   
+}
+
+ multiplicationTable(12);
