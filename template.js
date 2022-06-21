69b8421ac7a89f888a264c3e1fc834035dc807bd
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-21 18:41:28 +0600

    template js

---
+++ template.js
@@ -0,0 +1,9 @@
+
+var firstname = "Nur";
+var lastname = "Alam";
+
+var fullname1 = firstname + " " + lastname + " is a good boy.";
+var fullname2 = `${firstname} ${lastname} is a good boy.`;
+
+console.log(fullname1)
+console.log(fullname2)
