4b1ef168f547b795cab9d6930e87a30d51c5fc5e
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-23 21:29:02 +0600

    multiple line print through a single quote

--- template.js
+++ template.js
@@ -1,4 +1,6 @@
 
+//node template.js
+
 var firstname = "Nur";
 var lastname = "Alam";
 
@@ -7,3 +9,16 @@ var fullname2 = `${firstname} ${lastname} is a good boy.`;
 
 console.log(fullname1)
 console.log(fullname2)
+
+
+var multipleLine1 = "hello, this is Nur \n"
+                    +"I am a web designer\n"
+                    +"problem solver";
+
+var multipleLine2 = `hello, this is Nur 
+I am a web designer
+problem solver`;
+
+console.log(multipleLine1)
+console.log(multipleLine2)
+
