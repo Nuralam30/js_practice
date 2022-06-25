44cb5c9d7cd0976de5b29fc1ee17d36d536ed4b0
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-25 22:46:07 +0600

    split method

---
+++ splitMethod.js
@@ -0,0 +1,10 @@
+
+// splitMethod.js
+
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+
+console.log(myArray)
+
+
+// split(), splits a string into an array of substrings, and returns the array
