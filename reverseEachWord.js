e76c8e3992c37ece4f90b028731bfec72c80ac0a
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-25 22:41:13 +0600

    updated reverse word code

--- reverseEachWord.js
+++ reverseEachWord.js
@@ -1,14 +1,30 @@
 
 // reverseEachWord.js
-var sentence = "Hello this is Nura Alam ";
-var result = "";
-var temp = "";
+// var sentence = "Hello this is Nura Alam ";
+// var result = "";
+// var temp = "";
 
-for (var i=0; i<sentence.length; i++){
-    temp = sentence[i] + temp;
-    if(sentence[i] == " "){
-        result = result + temp;
-        temp = "";
-    }
+// for (var i=0; i<sentence.length; i++){
+//     temp = sentence[i] + temp;
+//     if(sentence[i] == " "){
+//         result = result + temp;
+//         temp = "";
+//     }
+// }
+
+// console.log(result);
+
+
+var string = "Welcome to this Javascript Guide!";
+
+// Output becomes !ediuG tpircsavaJ siht ot emocleW
+var reverseEntireSentence = reverseBySeparator(string, "");
+
+// Output becomes emocleW ot siht tpircsavaJ !ediuG
+var reverseEachWord = reverseBySeparator(reverseEntireSentence, " ");
+
+function reverseBySeparator(string, separator) {
+  return string.split(separator).reverse().join(separator);
 }
-console.log(result)
+
+console.log(reverseEachWord);
