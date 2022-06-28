0da1b3c6444eeae2f657d27a172e1a7320030fd9
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-28 23:07:49 +0600

    type of method working

---
+++ js viva/typeOf.js
@@ -0,0 +1,13 @@
+
+// typeOf.js
+
+var k = 1;
+if (1) {
+//   function foo() {};
+    const student = {
+        id : 1,
+        name : 'Nura Alam'
+    }
+    k += typeof student;
+}
+console.log(k);
