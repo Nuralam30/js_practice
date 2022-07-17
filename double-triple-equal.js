0f86f7f44d518977951f172ba9fe5e7877d1f461
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-17 22:36:06 +0600

    double vs triple equal

---
+++ double-triple-equal.js
@@ -0,0 +1,13 @@
+
+// double-triple-equal.js
+
+// n1 = 2 n2 ='2', n1=1 n2=true, n1=0 n2=false
+var n1 = 1;
+var n2 = true;
+
+if(n1 === n2){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
