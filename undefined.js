f03527624cc3a2894f49fad9a4d3695a4aedd95d
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-17 22:22:00 +0600

    undefined value create

---
+++ undefined.js
@@ -0,0 +1,24 @@
+
+let name;
+
+
+function getvalue(n1, n2){
+    console.log(n1 + n2) // use 'return n1 + n2;
+}
+var result = getvalue(12, 23)
+console.log(result)
+
+
+var person = {
+    name : 'Nur',
+    age : 24
+}
+console.log(person.hobby)
+
+
+var friends = ['Riad', 'Juyel'];
+console.log(friends[4])
+
+
+var friends = undefined;
+console.log(friends)
