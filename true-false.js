cf9ab07f46cd796b22e717a80b4c2179c8bbec8d
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-17 22:05:06 +0600

    falsy value in program

---
+++ true-false.js
@@ -0,0 +1,12 @@
+
+// true-false.js
+
+// falsy value   '', 0, null, false, undefined, NaN
+
+let name = false;
+if(name){
+    console.log('true value')
+}
+else{
+    console.log('false value')
+}
