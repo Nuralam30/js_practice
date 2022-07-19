5f949e77a18b1d87c04a79d854a20ecc4be0e40c
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-19 22:52:51 +0600

    closure js concept

---
+++ advanced js/closure.js
@@ -0,0 +1,23 @@
+
+// closure.js
+
+function counter(){
+    let count = 0;
+
+    return function(){
+        count++
+        return count;
+    }
+}
+
+const counter1 = counter()
+console.log(counter1())
+console.log(counter1())
+console.log(counter1())
+console.log(counter1())
+
+const counter2 = counter()
+console.log(counter2())
+console.log(counter2())
+console.log(counter1())
+console.log(counter1())
