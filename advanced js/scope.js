70d6a6e315ab06297f99e9bdc253d552656126df
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-19 22:39:06 +0600

    scope in value access

---
+++ advanced js/scope.js
@@ -0,0 +1,15 @@
+
+// scope.js
+
+function valueAccess(){
+    var id = 20;
+    if(id){
+        var friend = "Riya";
+        let hobby = "Journey";
+        // return friend
+    }
+    console.log(friend)
+    // console.log(hobby)
+}
+
+valueAccess();
