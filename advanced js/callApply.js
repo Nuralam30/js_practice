f66a3a90b50b4420f62d22bc0ee9f69fa81b2903
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-21 23:20:27 +0600

    bind mehtod uses

--- advanced js/callApply.js
+++ advanced js/callApply.js
@@ -5,11 +5,22 @@ const normalPerson = {
     salary : 20000,
     bonous : 2000,
     getSalary : function(amount){   //method
+        console.log(this)
         this.salary = this.salary + amount;
         return this.salary;
     }
 }
 
-const bonous = normalPerson.bonous;
-normalPerson.getSalary(bonous)
-console.log(normalPerson.salary)
+// const bonous = normalPerson.bonous;
+// normalPerson.getSalary(bonous)
+// console.log(normalPerson.salary)
+
+const specialPeron = {
+    name : 'Anderson',
+    salary : 28000
+}
+
+const specialPeronSalary = normalPerson.getSalary.bind(specialPeron);
+specialPeronSalary(3000);
+specialPeronSalary(3500);
+console.log(specialPeron.salary)
