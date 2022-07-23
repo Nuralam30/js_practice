44325fa0d148a1b14c29d4a68763a714a78fae90
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-07-23 20:42:16 +0600

    check wheather an input is valid date or not

--- advanced js/isDate.js
+++ advanced js/isDate.js
@@ -1,6 +1,6 @@
 
 function is_date(d){
-    if(isDate(d)){
+    if(d instanceof Date){
         return true
     }
     else{
@@ -8,4 +8,7 @@ function is_date(d){
     }
 }
 
-console.log(is_date("October 13, 2014 11:13:00"))
+console.log(is_date("October 13, 2014 11:13:00"));
+console.log(is_date(new Date(86400000)));
+console.log(is_date(new Date(99,5,24,11,33,30,0)));
+console.log(is_date([1, 2, 4, 0]));
