c8cf1e6c8fb2cc66e95f7109f593794c9456274c
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-08 22:44:06 +0600

    reduce in object

--- advanced js/reduce.js
+++ advanced js/reduce.js
@@ -4,3 +4,19 @@ let nums = [23, 24, 25, 35, 57];
 const total = nums.reduce( (sum, num) => sum + num , 0)
 
 console.log(total)
+
+// reduce in object
+
+let friends = [
+    {name: 'Babul', salary : 2000},
+    {name: 'Abul', salary : 3000},
+    {name: 'Mojid', salary : 2300},
+    {name: 'Faruk', salary : 2600},
+    {name: 'Kamal', salary : 2800}
+];
+
+const totalSalary = friends.reduce( (sum, friends) => {
+    return sum + friends.salary;
+}, 0);
+
+console.log(totalSalary)
