dc120b879b2df7da3146778be5a16b9796ca31f3
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-25 00:06:25 +0600

    page loader added

--- js animation/page loading/script.js
+++ js animation/page loading/script.js
@@ -1,19 +1,31 @@
-var opacity=0;
-var intervalID=0;
-window.onload=fadeout;
-    function fadeout(){
-           setInterval(hide, 200);
-    }
-function hide(){
-      var body=document.getElementById("body");
-      opacity =
-Number(window.getComputedStyle(body).getPropertyValue("opacity"))
+// var opacity=0;
+// var intervalID=0;
+// window.onload=fadeout;
+//     function fadeout(){
+//            setInterval(hide, 200);
+//     }
+// function hide(){
+//       var body=document.getElementById("page-loader");
+//       opacity =
+// Number(window.getComputedStyle(body).getPropertyValue("opacity"))
 
-        if(opacity>0){
-               opacity=opacity-0.1;
-                       body.style.opacity=opacity
-        }
-        else{
-            clearInterval(intervalID); 
-        }
-    }
+//         if(opacity>0){
+//                opacity=opacity-0.1;
+//                        body.style.opacity=opacity
+//         }
+//         else{
+//             clearInterval(intervalID); 
+//         }
+//     }
+
+
+window.onload = pageLoader;
+
+function pageLoader(){
+    setInterval( loaderActive, 5000)
+}
+
+function loaderActive(){
+    const loader = document.querySelector('.loader.container');
+    loader.classList.add('hide')
+}
