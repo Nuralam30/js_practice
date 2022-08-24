ee6f31cb20c857e5f1bb2e868e48352abf2d33fe
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-24 21:33:13 +0600

    fade in effect added in the page

---
+++ js animation/page loading/script.js
@@ -0,0 +1,19 @@
+var opacity=0;
+var intervalID=0;
+window.onload=fadeout;
+    function fadeout(){
+           setInterval(hide, 200);
+    }
+function hide(){
+      var body=document.getElementById("body");
+      opacity =
+Number(window.getComputedStyle(body).getPropertyValue("opacity"))
+
+        if(opacity>0){
+               opacity=opacity-0.1;
+                       body.style.opacity=opacity
+        }
+        else{
+            clearInterval(intervalID); 
+        }
+    }
